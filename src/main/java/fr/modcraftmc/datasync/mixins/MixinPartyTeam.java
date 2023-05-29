a1f99f499c8c1ce5dc2d405223ef10edeab1f19d
goldor <dimgoldoritri@gmail.com>
2023-05-29 11:51:18 +0200

    test mixins

---
+++ src/main/java/fr/modcraftmc/datasync/mixins/MixinPartyTeam.java
@@ -0,0 +1,33 @@
+package fr.modcraftmc.datasync.mixins;
+
+import com.mojang.authlib.GameProfile;
+import fr.modcraftmc.datasync.DataSync;
+import fr.modcraftmc.datasync.ftbsync.FTBSync;
+import net.minecraft.server.level.ServerPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Collection;
+
+@Mixin(value = dev.ftb.mods.ftbteams.data.PartyTeam.class, remap = false)
+public class MixinPartyTeam {
+    @Inject(at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbteams/data/PartyTeam;save()V") , method = "invite")
+    protected void onInvite(ServerPlayer from, Collection<GameProfile> players, CallbackInfoReturnable<Integer> cir){
+        DataSync.LOGGER.debug("received PartyTeam invite");
+        FTBSync.syncTeam((dev.ftb.mods.ftbteams.data.PartyTeam) (Object) this);
+    }
+
+    @Inject(at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbteams/data/TeamManager;syncTeamsToAll([Ldev/ftb/mods/ftbteams/data/Team;)V") , method = "promote")
+    protected void onPromote(ServerPlayer from, Collection<GameProfile> players, CallbackInfoReturnable<Integer> cir){
+        DataSync.LOGGER.debug("received PartyTeam promote");
+        FTBSync.syncTeam((dev.ftb.mods.ftbteams.data.PartyTeam) (Object) this);
+    }
+
+    @Inject(at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbteams/data/TeamManager;syncTeamsToAll([Ldev/ftb/mods/ftbteams/data/Team;)V") , method = "demote")
+    protected void onDemote(ServerPlayer from, Collection<GameProfile> players, CallbackInfoReturnable<Integer> cir){
+        DataSync.LOGGER.debug("received PartyTeam demote");
+        FTBSync.syncTeam((dev.ftb.mods.ftbteams.data.PartyTeam) (Object) this);
+    }
+}
---
+++ src/main/java/fr/modcraftmc/datasync/mixins/MixinTeam.java
@@ -0,0 +1,30 @@
+package fr.modcraftmc.datasync.mixins;
+
+import fr.modcraftmc.datasync.DataSync;
+import fr.modcraftmc.datasync.ftbsync.FTBSync;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.UUID;
+
+@Mixin(value = dev.ftb.mods.ftbteams.data.Team.class, remap = false)
+public class MixinTeam {
+    @Inject(at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbteams/data/TeamManager;syncTeamsToAll([Ldev/ftb/mods/ftbteams/data/Team;)V") , method = "denyInvite")
+    protected void onDenyInvite(CommandSourceStack source, CallbackInfoReturnable<Integer> cir){
+        DataSync.LOGGER.debug("received Team denyInvite");
+        FTBSync.syncTeam((dev.ftb.mods.ftbteams.data.Team) (Object) this);
+    }
+
+    @Inject(at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbteams/data/Team;getOnlineMembers()Ljava/util/List;") , method = "sendMessage")
+    protected void onSendMessage(UUID from, Component text, CallbackInfo ci){
+        DataSync.LOGGER.debug("received Team sendMessage");
+        FTBSync.syncTeam((dev.ftb.mods.ftbteams.data.Team) (Object) this);
+    }
+}

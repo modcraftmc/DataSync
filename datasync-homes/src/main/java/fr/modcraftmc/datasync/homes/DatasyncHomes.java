f8a1e30d9a88b19eca0c9fc5f63b633f3b33150e
manugame <manugameur.pro@gmail.com>
2023-08-22 19:01:44 +0200

    add crossservercore dependency TODO: use maven

--- datasync-homes/src/main/java/fr/modcraftmc/datasync/homes/DatasyncHomes.java
+++ datasync-homes/src/main/java/fr/modcraftmc/datasync/homes/DatasyncHomes.java
@@ -1,7 +1,12 @@
 package fr.modcraftmc.datasync.homes;
 
+import fr.modcraftmc.crossservercore.CrossServerCore;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("datasynchomes")
 public class DatasyncHomes {
+
+    public DatasyncHomes() {
+
+    }
 }

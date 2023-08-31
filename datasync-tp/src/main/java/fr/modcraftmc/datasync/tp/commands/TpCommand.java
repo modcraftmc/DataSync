d9702b492bf39df8554decfb562aa2b4ba1baa51
manugame <manugameur.pro@gmail.com>
2023-08-31 17:58:46 +0200

    fix tp command return value

--- datasync-tp/src/main/java/fr/modcraftmc/datasync/tp/commands/TpCommand.java
+++ datasync-tp/src/main/java/fr/modcraftmc/datasync/tp/commands/TpCommand.java
@@ -13,25 +13,23 @@ public class TpCommand extends CommandModule{
         COMMANDS.add(Commands.literal("tp")
                 .then(Commands.argument("target", NetworkPlayerArgument.networkPlayer())
                         .then(Commands.argument("player", NetworkPlayerArgument.networkPlayer())
-                                .executes(context -> {
-                                    tp(context.getSource(), NetworkPlayerArgument.getNetworkPlayer(context, "target"), NetworkPlayerArgument.getNetworkPlayer(context, "player"));
-                                    return 1;
-                                })
+                                .executes(context -> tp(context.getSource(), NetworkPlayerArgument.getNetworkPlayer(context, "target"), NetworkPlayerArgument.getNetworkPlayer(context, "player")))
                         )
-                        .executes(context -> {
-                            tp(context.getSource(), NetworkPlayerArgument.getNetworkPlayer(context, "target"));
-                            return 1;
-                        })
+                        .executes(context -> tp(context.getSource(), NetworkPlayerArgument.getNetworkPlayer(context, "target")))
                 ));
     }
 
-    private void tp(CommandSourceStack source, String player) {
-        if(!source.isPlayer())
+    private static int tp(CommandSourceStack source, String player) {
+        if(!source.isPlayer()) {
             source.sendFailure(Component.literal("You must be a player to use this command"));
+            return 0;
+        }
         new TpRequest(source.getPlayer().getName().getString(), player).fire();
+        return 1;
     }
 
-    private void tp(CommandSourceStack source, String playerTarget, String player) {
+    private int tp(CommandSourceStack source, String playerTarget, String player) {
         new TpRequest(player, playerTarget).fire();
+        return 1;
     }
 }

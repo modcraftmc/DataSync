a1dafee789357854f4cefcd0cef154254993038e
goldor <dimgoldoritri@gmail.com>
2025-03-02 18:15:30 +0100

    datasync ftbteams migration to neoforge 1.21.x (start)

--- datasync-ftbteams/src/main/java/fr/modcraftmc/datasync/ftbteams/TeamsSynchronizer.java
+++ datasync-ftbteams/src/main/java/fr/modcraftmc/datasync/ftbteams/TeamsSynchronizer.java
@@ -3,10 +3,12 @@ package fr.modcraftmc.datasync.ftbteams;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import dev.ftb.mods.ftbteams.FTBTeamsAPI;
+import com.mojang.authlib.GameProfile;
+import dev.ftb.mods.ftbteams.api.FTBTeamsAPI;
+import dev.ftb.mods.ftbteams.api.Team;
+import dev.ftb.mods.ftbteams.api.event.TeamEvent;
+import dev.ftb.mods.ftbteams.api.event.TeamManagerEvent;
 import dev.ftb.mods.ftbteams.data.*;
-import dev.ftb.mods.ftbteams.event.TeamEvent;
-import dev.ftb.mods.ftbteams.event.TeamManagerEvent;
 import dev.ftb.mods.ftbteams.net.SendMessageResponseMessage;
 import fr.modcraftmc.crossservercore.api.CrossServerCoreAPI;
 import fr.modcraftmc.crossservercore.api.events.CrossServerCoreReadyEvent;
@@ -19,14 +21,20 @@ import fr.modcraftmc.datasync.ftbteams.message.SyncTeamMessage;
 import fr.modcraftmc.datasync.ftbteams.message.SyncTeams;
 import fr.modcraftmc.datasync.ftbteams.serialization.SerializationUtil;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.ModList;
+import net.neoforged.fml.ModList;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.common.util.FakePlayer;
+import net.neoforged.neoforge.network.PacketDistributor;
+import net.neoforged.neoforge.server.ServerLifecycleHooks;
 import org.bson.Document;
 
 import java.io.IOException;
@@ -43,6 +51,10 @@ public class TeamsSynchronizer {
     public boolean FTBTeamsLoaded = false;
     public ISharedDataStore databaseTeamsData = new SharedDataStore(References.TEAMS_DATA_COLLECTION_NAME);
 
+    public static HolderLookup.Provider getLookupProvider(){
+        return (HolderLookup.Provider) ServerLifecycleHooks.getCurrentServer().registryAccess().asGetterLookup();
+    }
+
     public TeamsSynchronizer() {
         if(!ModList.get().isLoaded(References.FTBTEAMS_MOD_ID))
             return;
@@ -56,7 +68,7 @@ public class TeamsSynchronizer {
                 CrossServerCoreAPI.getAllPlayersOnCluster().forEach(TeamsSynchronizer::setPlayerTeamOnline);
             }
             else {
-                MinecraftForge.EVENT_BUS.addListener(this::loadTeamsWhenCSCReady); //if ftbteams is loaded before crossservercore, we need to wait for it to be ready
+                NeoForge.EVENT_BUS.addListener(this::loadTeamsWhenCSCReady); //if ftbteams is loaded before crossservercore, we need to wait for it to be ready
             }
         });
         TeamEvent.PROPERTIES_CHANGED.register((event) -> syncTeam(event.getTeam()));
@@ -70,7 +82,7 @@ public class TeamsSynchronizer {
         TeamEvent.CREATED.register((event) -> syncTeam(event.getTeam()));
         TeamEvent.DELETED.register((event) -> removeTeam(event.getTeam()));
 
-        MinecraftForge.EVENT_BUS.addListener(this::onPlayerJoinCluster);
+        NeoForge.EVENT_BUS.addListener(this::onPlayerJoinCluster);
     }
 
     private void loadTeamsWhenCSCReady(CrossServerCoreReadyEvent event){
@@ -78,7 +90,7 @@ public class TeamsSynchronizer {
     }
 
     private void onPlayerJoinCluster(PlayerJoinClusterEvent event){
-        if (!FTBTeamsAPI.isManagerLoaded()){
+        if (!FTBTeamsAPI.api().isManagerLoaded()){
             TeamManagerEvent.CREATED.register((e) -> {
                 setPlayerTeamOnline(event.getPlayer());
             });
@@ -89,71 +101,73 @@ public class TeamsSynchronizer {
     }
 
     public void removeTeam(Team team){
+        AbstractTeam abstractTeam = (AbstractTeam) team; //pray for it to be an abstract team
         if(!FTBTeamsLoaded) return;
-        DatasyncFtbTeam.LOGGER.debug(String.format("Removing team: %s", team.getDisplayName()));
-        CompoundTag teamsData = team.serializeNBT();
-        SyncTeams syncTeamsMessage = new SyncTeams(team.getId(), team.getType().name(), SerializationUtil.ToJsonElement(teamsData), true);
+        DatasyncFtbTeam.LOGGER.debug(String.format("Removing team: %s", abstractTeam.getDisplayName()));
+        CompoundTag teamsData = abstractTeam.serializeNBT(getLookupProvider());
+        SyncTeams syncTeamsMessage = new SyncTeams(abstractTeam.getId(), abstractTeam.getType().name(), SerializationUtil.ToJsonElement(teamsData), true);
 
         CrossServerCoreAPI.sendCrossMessageToAllOtherServer(syncTeamsMessage);
-        removeTeamFromDB(team);
+        removeTeamFromDB(abstractTeam);
     }
 
     public void syncTeam(Team team) {
+        AbstractTeam abstractTeam = (AbstractTeam) team; //pray for it to be an abstract team
         if(!FTBTeamsLoaded) return;
-        DatasyncFtbTeam.LOGGER.debug(String.format("Syncing team: %s", team.getDisplayName()));
+        DatasyncFtbTeam.LOGGER.debug(String.format("Syncing team: %s", abstractTeam.getDisplayName()));
         try{
-            CompoundTag teamsData = team.serializeNBT();
-            SyncTeams syncTeamsMessage = new SyncTeams(team.getId(), team.getType().name(), SerializationUtil.ToJsonElement(teamsData));
+            CompoundTag teamsData = abstractTeam.serializeNBT(getLookupProvider());
+            SyncTeams syncTeamsMessage = new SyncTeams(abstractTeam.getId(), abstractTeam.getType().name(), SerializationUtil.ToJsonElement(teamsData));
 
             CrossServerCoreAPI.sendCrossMessageToAllOtherServer(syncTeamsMessage);
         } catch (Exception e){
-            DatasyncFtbTeam.LOGGER.error(String.format("Error while syncing team: %s", team.getDisplayName()));
+            DatasyncFtbTeam.LOGGER.error(String.format("Error while syncing team: %s", abstractTeam.getDisplayName()));
         }
 
-        saveTeamToDB(team);
+        saveTeamToDB(abstractTeam);
     }
 
     public void handleTeamSync(SyncTeams syncTeamMessage){
         if(!FTBTeamsLoaded) return;
         CompoundTag teamsData = SerializationUtil.GetNbt(syncTeamMessage.teamsData);
-        Team team = FTBTeamsAPI.getManager().getTeamByID(syncTeamMessage.teamUUID);
+        AbstractTeam team = (AbstractTeam) FTBTeamsAPI.api().getManager().getTeamByID(syncTeamMessage.teamUUID).get();
         DatasyncFtbTeam.LOGGER.debug(String.format("team id: %s; team: %s", syncTeamMessage.teamUUID, team));
         if(team == null) {
             team = getNewTeam(syncTeamMessage.teamType, syncTeamMessage.teamUUID);
         }
-        team.deserializeNBT(teamsData);
+        team.deserializeNBT(teamsData, getLookupProvider());
 
-        Team finalTeam = team;
-        finalTeam.getMembers().forEach(uuid -> { //actualTeam is not serialized when a team is updated, so we need to do it manually
-            if(FTBTeamsAPI.getPlayerTeam(uuid) != finalTeam) {
-                FTBTeamsAPI.getManager().getInternalPlayerTeam(uuid).actualTeam = finalTeam;
+        AbstractTeam finalTeam = team;
+        finalTeam.getMembers().forEach(uuid -> { //effectiveTeam is not serialized when a team is updated, so we need to do it manually
+            if(FTBTeamsAPI.api().getManager().getTeamByID(uuid).get() != finalTeam) {
+                ((PlayerTeam) FTBTeamsAPI.api().getManager().getPlayerTeamForPlayerID(uuid).get()).setEffectiveTeam(finalTeam);
             }
         });
 
         if (syncTeamMessage.remove && team.getMembers().isEmpty()) {
-            team.manager.saveNow();
-            team.manager.getTeamMap().remove(team.getId());
+            TeamManagerImpl.INSTANCE.saveNow();
+            TeamManagerImpl.INSTANCE.getTeamMap().remove(team.getId());
             String fn = team.getId() + ".snbt";
 
             try {
-                Path dir = team.manager.server.getWorldPath(TeamManager.FOLDER_NAME).resolve("deleted");
+                Path dir = TeamManagerImpl.INSTANCE.getServer().getWorldPath(TeamManagerImpl.FOLDER_NAME).resolve("deleted");
                 if (Files.notExists(dir, new LinkOption[0])) {
                     Files.createDirectories(dir);
                 }
 
-                Files.move(team.manager.server.getWorldPath(TeamManager.FOLDER_NAME).resolve("party/" + fn), dir.resolve(fn));
+                Files.move(TeamManagerImpl.INSTANCE.getServer().getWorldPath(TeamManagerImpl.FOLDER_NAME).resolve("party/" + fn), dir.resolve(fn));
             } catch (IOException var10) {
                 var10.printStackTrace();
 
                 try {
-                    Files.deleteIfExists(team.manager.server.getWorldPath(TeamManager.FOLDER_NAME).resolve("party/" + fn));
+                    Files.deleteIfExists(TeamManagerImpl.INSTANCE.getServer().getWorldPath(TeamManagerImpl.FOLDER_NAME).resolve("party/" + fn));
                 } catch (IOException var9) {
                     var9.printStackTrace();
                 }
             }
         }
 
-        FTBTeamsAPI.getManager().syncTeamsToAll(team);
+        TeamManagerImpl.INSTANCE.syncToAll(team);
         if(team.getType() == TeamType.PLAYER) {
             ((PlayerTeam) team).updatePresence();
         }
@@ -175,12 +189,12 @@ public class TeamsSynchronizer {
     public void handleTeamMessage(UUID teamUUID, UUID from, Component message){
         DatasyncFtbTeam.LOGGER.debug(String.format("Received team message: %s", message.getString()));
         if(!FTBTeamsLoaded) return;
-        Team team = FTBTeamsAPI.getManager().getTeamByID(teamUUID);
+        AbstractTeam team = (AbstractTeam) FTBTeamsAPI.api().getManager().getTeamByID(teamUUID).get();
         DatasyncFtbTeam.LOGGER.debug(String.format("team id: %s; team: %s", teamUUID, team));
         if(team == null) return;
-        team.addMessage(new TeamMessage(from, System.currentTimeMillis(), message));
+        team.addMessage(new TeamMessageImpl(from, System.currentTimeMillis(), message));
         MutableComponent component = Component.literal("<");
-        component.append(team.manager.getName(from));
+        component.append(TeamManagerImpl.INSTANCE.getPlayerName(from));
         component.append(" @");
         component.append(team.getName());
         component.append("> ");
@@ -188,20 +202,20 @@ public class TeamsSynchronizer {
 
         for (ServerPlayer p : team.getOnlineMembers()) {
             p.displayClientMessage(component, false);
-            new SendMessageResponseMessage(from, message).sendTo(p);
+            PacketDistributor.sendToPlayer(p, new SendMessageResponseMessage(from, message));
         }
-        team.save();
+        team.markDirty();
     }
 
     public void saveTeamsToDB(){
         if(!FTBTeamsLoaded) return;
         DatasyncFtbTeam.LOGGER.debug("Saving teams");
-        FTBTeamsAPI.getManager().getTeams().forEach(team -> saveTeamToDB(team));
+        FTBTeamsAPI.api().getManager().getTeams().forEach(team -> saveTeamToDB(team));
     }
 
-    public void saveTeamToDB(Team team){
+    public void saveTeamToDB(AbstractTeam team){
         DatasyncFtbTeam.LOGGER.debug(String.format("Saving team: %s", team.getDisplayName()));
-        JsonObject teamData = SerializationUtil.ToJsonElement(team.serializeNBT()).getAsJsonObject();
+        JsonObject teamData = SerializationUtil.ToJsonElement(team.serializeNBT(getLookupProvider())).getAsJsonObject();
         Date date = new Date();
         String uuid = team.getId().toString();
         Document document = new Document("uuid", uuid)
@@ -222,16 +236,16 @@ public class TeamsSynchronizer {
         databaseTeamsData.accessOrThrow().find().forEach(data -> {
             JsonElement teamData = gson.fromJson(data.getString("teamData"), JsonElement.class);
             CompoundTag teamCompound = SerializationUtil.GetNbt(teamData);
-            Team team = FTBTeamsAPI.getManager().getTeamByID(UUID.fromString(teamCompound.getString("id")));
+            AbstractTeam team = (AbstractTeam) FTBTeamsAPI.api().getManager().getTeamByID(UUID.fromString(teamCompound.getString("id"))).get();
             if(team == null) {
                 team = getNewTeam(teamCompound.getString("type"), UUID.fromString(teamCompound.getString("id")));
             }
-            team.deserializeNBT(teamCompound);
+            team.deserializeNBT(teamCompound, getLookupProvider());
         });
     }
 
-    private static Team getNewTeam(String teamType, UUID teamId){
-        Team team;
+    private static AbstractTeam getNewTeam(String teamType, UUID teamId){
+        AbstractTeam team;
         TeamType type = null;
         switch (teamType){
             case "party", "PARTY" -> type = TeamType.PARTY;
@@ -242,27 +256,19 @@ public class TeamsSynchronizer {
 
         switch (type){
             case PARTY -> {
-                team = new PartyTeam(FTBTeamsAPI.getManager());
+                team = new PartyTeam(TeamManagerImpl.INSTANCE, teamId);
             }
             case SERVER -> {
-                team = new ServerTeam(FTBTeamsAPI.getManager());
+                team = new ServerTeam(TeamManagerImpl.INSTANCE, teamId);
             }
             case PLAYER -> {
-                team = new PlayerTeam(FTBTeamsAPI.getManager());
-                FTBTeamsAPI.getManager().getKnownPlayers().put(teamId, (PlayerTeam) team);
+                team = new PlayerTeam(TeamManagerImpl.INSTANCE, teamId);
             }
             default -> {
                 return null;
             }
         }
-        try {
-            Field idField = TeamBase.class.getDeclaredField("id");
-            idField.setAccessible(true);
-            idField.set(team, teamId);
-        } catch (IllegalAccessException | NoSuchFieldException e) {
-            DatasyncFtbTeam.LOGGER.error("Error while setting team id");
-        }
-        FTBTeamsAPI.getManager().getTeamMap().put(teamId, team);
+        FTBTeamsAPI.api().getManager().getTeams().put(teamId, team);
         return team;
     }
 

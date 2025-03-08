54fa2e8ea377b839ae403cafa8d4836e7f82dfdc
goldor <dimgoldoritri@gmail.com>
2025-03-09 00:46:05 +0100

    fix datasync ftbteams mixins

--- datasync-ftbteams/src/main/java/fr/modcraftmc/datasync/ftbteams/mixin/MixinPartyTeam.java
+++ datasync-ftbteams/src/main/java/fr/modcraftmc/datasync/ftbteams/mixin/MixinPartyTeam.java
@@ -14,7 +14,7 @@ import java.util.Collection;
 
 @Mixin(value = dev.ftb.mods.ftbteams.data.PartyTeam.class, remap = false)
 public class MixinPartyTeam {
-    @Inject(at = @At(value = "INVOKE_ASSIGN", target = "Ldev/ftb/mods/ftbteams/data/FTBTUtils;getPlayerByUUID(Lnet/minecraft/server/MinecraftServer;Ljava/util/UUID;)Lnet/minecraft/server/level/ServerPlayer;") , method = "invite")
+    @Inject(at = @At(value = "INVOKE", target = "Ljava/util/Collection;iterator()Ljava/util/Iterator;") , method = "invite")
     protected void onInvite(ServerPlayer from, Collection<GameProfile> players, CallbackInfoReturnable<Integer> cir){
         DatasyncFtbTeam.LOGGER.debug("FTBTeams PartyTeam inviting");
         PartyTeam team = (PartyTeam) (Object) this;
@@ -24,13 +24,13 @@ public class MixinPartyTeam {
         }
     }
 
-    @Inject(at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbteams/data/TeamManager;syncTeamsToAll([Ldev/ftb/mods/ftbteams/data/Team;)V") , method = "promote")
+    @Inject(at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbteams/data/TeamManagerImpl;syncToAll([Ldev/ftb/mods/ftbteams/api/Team;)V") , method = "promote")
     protected void onPromote(ServerPlayer from, Collection<GameProfile> players, CallbackInfoReturnable<Integer> cir){
         DatasyncFtbTeam.LOGGER.debug("FTBTeams PartyTeam promoting");
         DatasyncFtbTeam.teamsSynchronizer.syncTeam((dev.ftb.mods.ftbteams.data.PartyTeam) (Object) this);
     }
 
-    @Inject(at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbteams/data/TeamManager;syncTeamsToAll([Ldev/ftb/mods/ftbteams/data/Team;)V") , method = "demote")
+    @Inject(at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbteams/data/TeamManagerImpl;syncToAll([Ldev/ftb/mods/ftbteams/api/Team;)V") , method = "demote")
     protected void onDemote(ServerPlayer from, Collection<GameProfile> players, CallbackInfoReturnable<Integer> cir){
         DatasyncFtbTeam.LOGGER.debug("FTBTeams PartyTeam demoting");
         DatasyncFtbTeam.teamsSynchronizer.syncTeam((dev.ftb.mods.ftbteams.data.PartyTeam) (Object) this);

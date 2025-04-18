2d812774308a0a54d253f5b0d1ea1c83951ccc6d
manugame <braibant.manu@gmail.com>
2025-04-18 21:07:35 +0200

    fix(inventory): correctly serialize player data

--- datasync-inventory/src/main/java/fr/modcraftmc/datasync/inventory/serialization/SerializationUtil.java
+++ datasync-inventory/src/main/java/fr/modcraftmc/datasync/inventory/serialization/SerializationUtil.java
@@ -2,17 +2,18 @@ package fr.modcraftmc.datasync.inventory.serialization;
 
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.NbtUtils;
+import net.minecraft.nbt.SnbtPrinterTagVisitor;
+import net.minecraft.nbt.TagParser;
 
 
 public class SerializationUtil {
     public static String ToString(CompoundTag nbt){
-        return NbtUtils.structureToSnbt(nbt);
+        return new SnbtPrinterTagVisitor().visit(nbt);
     }
 
     public static CompoundTag ToNbt(String snbt){
         try {
-            return NbtUtils.snbtToStructure(snbt);
+            return TagParser.parseTag(snbt);
         } catch (CommandSyntaxException e) {
             e.printStackTrace();
             return null;
